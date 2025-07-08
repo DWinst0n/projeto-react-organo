06aa37571950c8507d04c76b7eae4383dfd6913f
DWinst0n <daviwinstonsm@gmail.com>
2025-07-07 22:08:06 -0300

    Adição da estruturação do cabeçalho

---
+++ src/header.js
@@ -0,0 +1,31 @@
+import "./css/header.css";
+import logo from "./img/logo.png";
+import circulo1 from "./img/circulo1.png";
+import circulo2 from "./img/circulo2.png";
+import triangulo from "./img/triangulo.png";
+import imagemEquipe from "./img/Imagem duotone.png";
+
+function App() {
+	return (
+		<header className="app__header">
+			<img src={logo} className="logo" alt="logo" />
+			<div className="formas__container">
+				<img src={circulo1} className="forma__tipo1" alt="" id="circulo1" />
+
+				<img src={triangulo} className="forma__tipo1" alt="" id="triangulo1" />
+
+				<img src={triangulo} className="forma__tipo2" alt="" id="triangulo2" />
+
+				<img src={circulo2} className="forma__tipo2" alt="" id="circulo2" />
+			</div>
+
+			<div className="titulo__container">
+				<h1 className="titulo__principal">Pessoas e times</h1>
+				<h2 className="titulo__secundario">organizados em um só lugar!</h2>
+			</div>
+			<img src={imagemEquipe} className="imagem__equipe" alt="" />
+		</header>
+	);
+}
+
+export default App;
