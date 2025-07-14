8e1cae9c351b06320ab20312470cf5839d3139c1
DWinst0n <daviwinstonsm@gmail.com>
2025-07-14 19:31:29 -0300

    Adiciona componente CampoSelect no formulário

---
+++ src/components/Form/CampoSelect.jsx
@@ -0,0 +1,21 @@
+const CampoSelect = (p) => {
+	return (
+		<div className="campo__form">
+			<label htmlFor="selecionarCargo" className="dado__tipo">
+				{p.dado}
+			</label>
+			<select name="" id="selecionarCargo" className="form__select" defaultValue="">
+				<option value="" disabled>
+					Selecione o time:
+				</option>
+				{p.times.map((equipe) => (
+					<option key={equipe} value={equipe}>
+						{equipe}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
+
+export default CampoSelect;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,14 +1,20 @@
 import { CampoArquivo } from "./CampoArquivo";
+import CampoSelect from "./CampoSelect";
 import { CampoTexto } from "./CampoTexto";
+import { Botao } from "./Botao";
 import "./form.css";
 const Form = () => {
+	const times = ["Programação", "Front-End", "Data Science", "Devops", "UX e Design", "Mobile", "Inovação e Gestão"];
 	return (
 		<section className="form__container">
 			<form className="formulario">
 				<h3>Preencha os dados para criar o card do colaborador.</h3>
-				<CampoTexto dado="Nome" />
-				<CampoTexto dado="Cargo" />
+				<CampoTexto dado="Nome" requered />
+				<CampoTexto dado="Cargo" requered />
 				<CampoArquivo />
+				<CampoSelect dado="Time" times={times} requered />
+
+				<Botao>Criar card</Botao>
 			</form>
 		</section>
 	);
