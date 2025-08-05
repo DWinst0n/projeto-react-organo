1b4efebbbbb28959d9523be81e99708a73917a32
DWinst0n <daviwinstonsm@gmail.com>
2025-08-04 21:39:37 -0300

    Criação de um componente com conjunto de formas animadas com suas variações

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,23 +1,14 @@
 import "./banner.css";
 import logo from "../../img/logo.png";
-import circulo1 from "../../img/circulo1.png";
-import circulo2 from "../../img/circulo2.png";
-import triangulo from "../../img/triangulo.png";
 import imagembanner from "../../img/Imagem duotone.png";
+import Formas from "./Formas";
 
 export const Banner = () => {
 	return (
 		<header className="app__header">
 			<img src={logo} className="logo" alt="logo" />
-			<div className="formas__container">
-				<img src={circulo1} className="forma__tipo1" alt="" id="circulo1" />
 
-				<img src={triangulo} className="forma__tipo1" alt="" id="triangulo1" />
-
-				<img src={triangulo} className="forma__tipo2" alt="" id="triangulo2" />
-
-				<img src={circulo2} className="forma__tipo2" alt="" id="circulo2" />
-			</div>
+			<Formas componente={"banner"} />
 
 			<div className="titulo__container">
 				<h1 className="titulo__principal">Pessoas e times</h1>
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,11 +1,9 @@
 import "./footer.css";
 import logo from "../../img/logo.png";
-import circulo1 from "../../img/circulo1.png";
-import circulo2 from "../../img/circulo2.png";
-import triangulo from "../../img/triangulo.png";
 import iconeFb from "../../img/fb.png";
 import iconeTw from "../../img/tw.png";
 import iconeIg from "../../img/ig.png";
+import Formas from "../Banner/Formas";
 
 const Footer = () => {
 	return (
@@ -15,15 +13,7 @@ const Footer = () => {
 				<img src={iconeTw} alt="Icone com a logo do X" className="icone__footer" />
 				<img src={iconeIg} alt="Icone com a logo do Instagram" className="icone__footer" />
 			</div>
-			<div className="formas__container footer__shape">
-				<img src={circulo1} className="forma__tipo1" alt="" id="circulo3" />
-
-				<img src={triangulo} className="forma__tipo1" alt="" id="triangulo3" />
-
-				<img src={triangulo} className="forma__tipo2" alt="" id="triangulo4" />
-
-				<img src={circulo2} className="forma__tipo2" alt="" id="circulo4" />
-			</div>
+			<Formas componente={"footer"} />
 			<img src={logo} className="logo__footer" alt="logo" />
 			<span>Desenvolvido por Davi Winston, estudante Alura</span>
 		</footer>
